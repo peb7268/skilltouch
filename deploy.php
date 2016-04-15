274bc8bbf75e44018d5af6e996c0f91398ad3839
Paul Barrick <peb7268@gmail.com>
2016-04-14 22:01:50 -0400

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -3,8 +3,8 @@
 require 'recipe/wordpress.php';
 
 server('prod', '66.147.244.164', 22)
-->user('reloboxc')
-->password('Invest1994!')
+->user('')
+->password('')
 ->stage('production')
 ->env('deploy_path', '/home5/reloboxc/public_html/skilltouch.com/wp-content/themes'); // Define the base path to deploy your project to.
 
